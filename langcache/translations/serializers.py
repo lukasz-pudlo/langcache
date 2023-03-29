e1c33b1a99898382cbd443fd742b42cf61315e64
lukasz-pudlo <l.pudlo1990@gmail.com>
2023-03-29 21:37:38 +0100

    Add custom create method in TranslationSerializer

--- langcache/translations/serializers.py
+++ langcache/translations/serializers.py
@@ -16,10 +16,31 @@ class PhraseSerializer(serializers.ModelSerializer):
 
 
 class TranslationSerializer(serializers.ModelSerializer):
-    user = serializers.StringRelatedField()
     source_phrase = PhraseSerializer()
     target_phrase = PhraseSerializer()
 
     class Meta:
         model = Translation
         fields = ['id', 'user', 'source_phrase', 'target_phrase']
+
+    def create(self, validated_data):
+        source_phrase_data = validated_data.pop('source_phrase')
+        target_phrase_data = validated_data.pop('target_phrase')
+
+        source_language = Language.objects.get(
+            pk=source_phrase_data['language'].id)
+        target_language = Language.objects.get(
+            pk=target_phrase_data['language'].id)
+
+        source_phrase = Phrase.objects.create(
+            text=source_phrase_data['text'], language=source_language)
+        target_phrase = Phrase.objects.create(
+            text=target_phrase_data['text'], language=target_language)
+
+        translation = Translation.objects.create(
+            user=validated_data['user'],
+            source_phrase=source_phrase,
+            target_phrase=target_phrase
+        )
+
+        return translation
