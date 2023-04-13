da5c24cfffd8f558cc1845894ddc47ac434944d3
lukasz-pudlo <l.pudlo1990@gmail.com>
2023-04-13 22:11:47 +0100

    Put searchbar input and button in a row

--- langcache-frontend/src/components/Searchbar.js
+++ langcache-frontend/src/components/Searchbar.js
@@ -14,20 +14,24 @@ const Searchbar = ({ onSearch }) => {
 
   return (
     <div className="searchbar-wrapper">
-      <form onSubmit={handleSubmit}>
-        <div className="form-group">
-          <input
-            type="text"
-            value={searchTerm}
-            onChange={handleSearchChange}
-            className="form-control"
-            id="search-term"
-            maxLength="510"
-            placeholder="Search for translations..."
-          />
-        </div>
-        <button type="submit">Search</button>
-      </form>
+        <form onSubmit={handleSubmit}>
+            <div className="searchbar-grid">
+            <div className="form-group">
+                <input
+                type="text"
+                value={searchTerm}
+                onChange={handleSearchChange}
+                className="form-control"
+                id="search-term"
+                maxLength="510"
+                placeholder="Search for translations..."
+                />
+            </div>
+                <div className="search-button-container">
+                    <button type="submit">Search</button>
+                </div>
+            </div>
+        </form>
     </div>
   );
 };
