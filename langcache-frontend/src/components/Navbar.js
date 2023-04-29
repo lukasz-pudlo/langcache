5974a2a3d41e48977d512d747e3852ad8fea48be
lukasz-pudlo <l.pudlo1990@gmail.com>
2023-04-29 09:31:04 +0100

    Start adding Bootstrap to Navbar

--- langcache-frontend/src/components/Navbar.js
+++ langcache-frontend/src/components/Navbar.js
@@ -6,22 +6,33 @@ const Navbar = () => {
   const location = useLocation();
 
     return (
-      <header>
-        <nav>
-          <ul>
-            <li>
-            <NavLink to="/upload_text" className={location.pathname === '/upload_text' ? 'active-link' : ''}>
-              Text
-            </NavLink>
-            </li>
-            <li>
-            <NavLink to="/translations" className={location.pathname === '/translations' ? 'active-link' : ''}>
-              Translations
-            </NavLink>
+      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <NavLink to="/upload_text" className="nav-link" activeClassName="active-link">
+                Text
+              </NavLink>
             </li>
           </ul>
-        </nav>
-      </header>
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <NavLink to="/translations" className="nav-link" activeClassName="active-link">
+                Translations
+              </NavLink>
+            </li>
+          </ul>
+            <form class="d-flex" role="search">
+              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+              <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+          </div>
+        </div>
+      </nav>
     );
   };
   
