abd2d9aaa4ddd8fa51bda35fc8207ca41d7862a9
lukasz-pudlo <l.pudlo1990@gmail.com>
2023-04-30 18:34:06 +0100

    Remove React Bootstrap elements from Navbar

--- langcache-frontend/src/components/Navbar.js
+++ langcache-frontend/src/components/Navbar.js
@@ -1,40 +1,32 @@
 import React from 'react';
-import { NavLink, useLocation } from 'react-router-dom';
-
+import { useLocation } from 'react-router-dom';
 
 const Navbar = () => {
   const location = useLocation();
 
-    return (
-      <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+  return (
+    <nav className="navbar navbar-expand-lg bg-light">
+      <div className="container-fluid">
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <NavLink to="/upload_text" className="nav-link" activeClassName="active-link">
-                Text
-              </NavLink>
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="/upload_text">Text</a>
+              </li>
             </li>
-          </ul>
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <NavLink to="/translations" className="nav-link" activeClassName="active-link">
-                Translations
-              </NavLink>
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="/translations">Translations</a>
+              </li>
             </li>
           </ul>
-            <form class="d-flex" role="search">
-              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-              <button class="btn btn-outline-success" type="submit">Search</button>
-            </form>
-          </div>
         </div>
-      </nav>
-    );
-  };
-  
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
